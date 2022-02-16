8fc39ab8f8aacdecd8242b825cb1e787697ca350
Matthew Rognstad <rognstad@gmail.com>
2022-02-16 02:47:05 -0600

    Add some more info to the about page

--- _build/src/pages/About.jsx
+++ _build/src/pages/About.jsx
@@ -5,12 +5,16 @@ export default () => (
     <h1>About TI4 rules</h1>
     <p>This is a free tool to make it easier to lookup and share the rules for Twilight Imperium 4th Edition.</p>
 
-    <h4>Who made this?</h4>
+    <h4>What is it based on?</h4>
+    <p>It is based on v2.0 of the <a href="https://images-cdn.fantasyflightgames.com/filer_public/51/55/51552c7f-c05c-445b-84bf-4b073456d008/ti10_pok_living_rules_reference_20_web.pdf" target="_blank">Living Rules Reference</a>.</p>
 
+    <h4>I have a question that isn't answered here. What can I do?</h4>
+    <p>You might also want to TI4 designer Dane Beltrami's <a href="https://docs.google.com/document/d/1_93kw7KkzvAXoQAxRioxo7iAt-Ar6BMIxdNf4Lv25rI/edit" target="_blank">answers to FAQs about Prophecy of Kings</a>. These answers haven't been incorporated into an LRR update yet but are considered official.</p>
+
+    <h4>Who made this?</h4>
     <p>This was created by <a href='https://scoopsy.com'>Scott Parker</a>.</p>
 
     <h4>I think you're claiming ownership of this intellectual property!</h4>
-
     <p>Ha! Twilight Imperium, Mecatol Rex, and all associated works remain the copyright of their respective copyright holders.</p>
 
     <h4>License</h4>
